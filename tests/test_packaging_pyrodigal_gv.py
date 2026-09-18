fa31e89d4eca6847cd8021170a7e7a14b39f43f2
Emmanuel Nnadi <eennadi@gmail.com>
2026-09-18 19:34:20 +0100

    fix: support Python 3.10 packaging regression test

--- tests/test_packaging_pyrodigal_gv.py
+++ tests/test_packaging_pyrodigal_gv.py
@@ -1,6 +1,9 @@
 import subprocess
 import sys
-import tomllib
+try:
+    import tomllib
+except ModuleNotFoundError:  # Python 3.10 compatibility
+    import tomli as tomllib
 from pathlib import Path
 
 import pytest
